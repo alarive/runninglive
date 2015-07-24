13b226c2745d46d49317627f23c1ee6238d0f88f
Aurelien Larive <alarive@imj.fr>
2015-07-24 22:07:59 +0200

    Disable CSRF

--- src/main/java/com/runninglive/security/WebSecurityConfiguration.java
+++ src/main/java/com/runninglive/security/WebSecurityConfiguration.java
@@ -29,6 +29,8 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/**").hasRole("USER").and().httpBasic();
+        http.authorizeRequests().antMatchers("/**").hasRole("USER")
+            .and().httpBasic()
+            .and().csrf().disable();
     }
 }

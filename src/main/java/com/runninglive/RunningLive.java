ef2b4e3d8140594b3347b01c083e5b6dd248913d
Aurelien Larive <alarive@imj.fr>
2015-07-24 21:08:09 +0200

    Output ISO8601 dates

--- src/main/java/com/runninglive/RunningLive.java
+++ src/main/java/com/runninglive/RunningLive.java
@@ -1,9 +1,13 @@
 package com.runninglive;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JSR310Module;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 
 /**
  * User: alarive
@@ -20,4 +24,12 @@ public class RunningLive extends SpringBootServletInitializer {
         SpringApplication.run(RunningLive.class, args);
     }
 
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.configure(SerializationFeature.INDENT_OUTPUT, true);
+        objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+        objectMapper.registerModule(new JSR310Module());
+        return objectMapper;
+    }
 }
